c1f7a57115f402a43a1a8ebe5a0c9372cee49d1c
negset <15167516+negset@users.noreply.github.com>
2025-07-13 22:19:28 +0900

    update copyright year

--- js/script.js
+++ js/script.js
@@ -1,3 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
   M.Tabs.init(document.querySelector('.tabs'));
+
+  let copyright = document.querySelector(".footer-copyright");
+  let year = new Date().getFullYear();
+  copyright.textContent = `© ${year} negset`;
 });
